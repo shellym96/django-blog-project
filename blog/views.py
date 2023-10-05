8216548223f0681c26f5252a191f41610f9e6426
shellym96 Maughan <shellymaughan10@gmail.com>
2023-10-05 17:13:33 +0000

    Add login and signup confirmation

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -20,12 +21,14 @@ class PostDetail(View):
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-        return render (
+            return render(
             request,
             "post_detail.html",
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )
